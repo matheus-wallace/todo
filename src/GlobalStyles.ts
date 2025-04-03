1b4a17ff54fad72f12c38c3323c76036146ef617
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-04-03 19:21:27 -0300

    feat: inter font

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+
+  *{
+    font-family: 'Inter', sans-serif;
+  }
+
   body {
     margin: 0;
     padding: 0;
