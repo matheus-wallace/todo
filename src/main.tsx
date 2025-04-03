eeae00c99bdc7abe11adfc6b0df02af2fc7e9cb7
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-04-03 17:54:09 -0300

    feat: add description and motivations to make the project

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { GlobalStyles } from './GlobalStyles.ts';
 import { ThemeProvider } from 'styled-components';
-import { theme } from './defaultTheme.ts';
+import { theme } from './components/themes/defaultTheme.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
